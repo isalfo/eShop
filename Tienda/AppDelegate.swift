f00165ab3fb8e14d13dcae154df6002d1eed526b
Gonzalo Alfonso <gonzalo.alfonso@globant.com>
2021-09-20 18:27:23 -0300

    Add Stripe

--- Tienda/AppDelegate.swift
+++ Tienda/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Firebase
+import Stripe
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
+    
+    StripeAPI.defaultPublishableKey = "pk_test_51JbthIE3LuLqyUPom4BQiSTZUmBpQ9QQpSfUwyiD2aY1tlAzy0cQsFtGW1dJoqdoAJMmHs3SdrRwyM8owkm4OFBd00Wl8D6RPL"
+    FirebaseApp.configure()
+    
+    
     return true
   }
 }
