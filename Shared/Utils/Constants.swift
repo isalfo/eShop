8058ff58592370dbbdf2d3c8c6850c9ec8fb1941
Gonzalo Alfonso <gonzalo.alfonso@globant.com>
2021-09-21 10:46:08 -0300

    Add constants file

---
+++ Shared/Utils/Constants.swift
@@ -0,0 +1,28 @@
+//
+//  Constants.swift
+//  Tienda
+//
+//  Created by Gonzalo Alfonso on 21/09/2021.
+//
+
+import Foundation
+import UIKit
+
+struct Storyboard {
+  static let LoginStoryboard = "LoginStoryboard"
+}
+
+struct StoryboardID {
+  static let LoginVC = "loginVC"
+}
+
+struct AppImages {
+  static let GreenCheck = "green_check"
+  static let RedCheck = "red_check"
+}
+
+struct AppColors {
+  static let Blue = #colorLiteral(red: 0.2274509804, green: 0.2666666667, blue: 0.3607843137, alpha: 1)
+  static let Red = #colorLiteral(red: 0.8352941176, green: 0.3921568627, blue: 0.3137254902, alpha: 1)
+  static let OffWhite = #colorLiteral(red: 0.9529411765, green: 0.9490196078, blue: 0.968627451, alpha: 1)
+}
