45692f55a88a1ff00256ebd996b2fbeaf565a2ad
Gonzalo Alfonso <gonzalo.alfonso@globant.com>
2021-09-27 10:01:15 -0300

    Change login UI

--- Shared/View/RoundedViews.swift
+++ Shared/View/RoundedViews.swift
@@ -12,6 +12,8 @@ class RoundedButton: UIButton {
   override func awakeFromNib() {
     super.awakeFromNib()
     layer.cornerRadius = 5
+    layer.borderColor = UIColor.white.cgColor
+    layer.borderWidth = 2
   }
 }
 
